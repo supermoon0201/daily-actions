4fcdc3ecb5d67c17ad459b510ea42c4af24430a5
supermoon0201 <supermoon0201@163.com>
2025-07-09 21:05:08 +0800

    nodeseek添加头

--- nodeseek/nodeseek.py
+++ nodeseek/nodeseek.py
@@ -30,6 +30,7 @@ headers = {
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36',
+    'Content-Type': 'application/json',
 }
 
 # Iterate over multiple account cookies for check-in
